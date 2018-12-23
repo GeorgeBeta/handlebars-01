b7e50d3f9231045e414beb5e501b7b71ad46fd36
GeorgeBeta <jorge@zaporta.com>
2018-12-23 22:02:56 +0100

    HBS Funcionamiento Basico

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const app = express();
+const hbs = require('hbs');
 
 app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'hbs');
 
-// app.get('/', function(req, res) {
-//     // res.send('Hola Mundo')
-//     let salida = {
-//         nombre: 'Jorge',
-//         Edad: 60,
-//         url: req.url
-//     }
-//     res.send(salida);
-// })
+app.get('/', function(req, res) {
+    res.render('home', { nombre: 'Jorge', anio: new Date().getFullYear() });
+})
+
+app.get('/about', function(req, res) {
+    res.render('about');
+})
 
 app.listen(3000, () => {
     console.log('Escuchando el puerto :', 3000);
