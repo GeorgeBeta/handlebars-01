4c8597bc6af78b5f274c6dd8d674761c5019f6b7
GeorgeBeta <jorge@zaporta.com>
2018-12-23 23:41:34 +0100

    HBS Partials y Variables

--- server.js
+++ server.js
@@ -3,14 +3,20 @@ const app = express();
 const hbs = require('hbs');
 
 app.use(express.static(__dirname + '/public'));
+hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 
 app.get('/', function(req, res) {
-    res.render('home', { nombre: 'Jorge', anio: new Date().getFullYear() });
+    res.render('home', {
+        nombre: 'Jorge',
+        anio: new Date().getFullYear()
+    });
 })
 
 app.get('/about', function(req, res) {
-    res.render('about');
+    res.render('about', {
+        anio: new Date().getFullYear()
+    });
 })
 
 app.listen(3000, () => {
